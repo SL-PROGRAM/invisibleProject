30f6bbba6044398ec25b97849e91aa0b52ce79b1
sl <s.leyral@gmail.com>
2020-08-10 09:56:05 +0200

    Creation de toute les Activity

---
+++ app/src/main/java/com/example/devinet/activities/AProposActivity.java
@@ -0,0 +1,16 @@
+package com.example.devinet.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.devinet.R;
+
+public class AProposActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a_propos);
+    }
+}
