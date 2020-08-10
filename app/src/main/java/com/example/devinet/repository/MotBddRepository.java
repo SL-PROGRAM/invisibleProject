b54c6345b9e712d15c6789dd0450debd06b3bc7c
sl <s.leyral@gmail.com>
2020-08-10 11:19:07 +0200

    add MotRepository

--- app/src/main/java/com/example/devinet/repository/MotBddRepository.java
+++ app/src/main/java/com/example/devinet/repository/MotBddRepository.java
@@ -1,4 +1,84 @@
 package com.example.devinet.repository;
 
-public class MotBddRepository {
+import android.content.Context;
+import android.os.AsyncTask;
+
+import androidx.lifecycle.LiveData;
+
+import com.example.devinet.bo.Mot;
+import com.example.devinet.dal.MotDAO;
+
+import java.util.List;
+
+public class MotBddRepository implements IMotRepository {
+
+    private MotDAO motDAO;
+
+    public MotBddRepository(Context context) {
+        com.example.achat.dal.AppDatabase bdd = com.example.achat.dal.AppDatabase.getInstanceMot(context);
+        motDAO = bdd.getMotDAO();
+    }
+
+    public void insert(final Mot mot) {
+        new AsyncTask<Mot, Void, Void>() {
+            @Override
+            protected Void doInBackground(Mot... mots) {
+                motDAO.insert(mots[0]);
+                return null;
+            }
+        }.execute(mot);
+    }
+
+    List<Mot> resultat = null;
+
+    @Override
+    public LiveData<List<Mot>> get() {
+        return motDAO.get();
+    }
+
+    @Override
+    public Mot get(int id) {
+        return motDAO.get(id);
+    }
+
+    @Override
+    public void update(Mot article) {
+        //Je créé un nouveau thread(ouvrier)
+        new AsyncTask<Mot, Void, Void>() {
+            //Je lui dis quoi faire
+            @Override
+            protected Void doInBackground(Mot... mots) {
+                //Inserer un Article dans la bdd
+                motDAO.update(mots[0]);
+                return null;
+            }//Je lui dis de le faire
+        }.execute(article);
+    }
+
+    @Override
+    public void delete(Mot mot) {
+        new AsyncTask<Mot, Void, Void>() {
+            @Override
+            protected Void doInBackground(Mot... mots) {
+                //Delete dans la base de données.
+                motDAO.delete(mots[0]);
+                return null;
+            }
+        }.execute(mot);
+    }
+
+    @Override
+    public void delete() {
+        //Je créé un nouveau thread(ouvrier)
+        new AsyncTask<Void, Void, Void>() {
+            //Je lui dis quoi faire
+            @Override
+            protected Void doInBackground(Void... voids) {
+                //Inserer un Article dans la bdd
+                motDAO.delete();
+                return null;
+            }//Je lui dis de le faire
+        }.execute();
+    }
 }
+
