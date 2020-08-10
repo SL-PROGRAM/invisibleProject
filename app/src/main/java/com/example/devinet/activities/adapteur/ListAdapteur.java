6b6022af36f20efddd0bc624e756bbbc2c339b5e
sl <s.leyral@gmail.com>
2020-08-10 15:50:26 +0200

    ADD adapter

---
+++ app/src/main/java/com/example/devinet/activities/adapteur/ListAdapteur.java
@@ -0,0 +1,6 @@
+package com.example.devinet.activities.adapteur;
+
+public class ListAdapteur {
+
+    //Faire adapter pour les != listes
+}
---
+++ app/src/main/java/com/example/devinet/activities/adapteur/NiveauAdapteur.java
@@ -0,0 +1,6 @@
+package com.example.devinet.activities.adapteur;
+
+public class NiveauAdapteur {
+    //Faire adapter pour liste des niveaux (nbre de lettres)
+
+}
