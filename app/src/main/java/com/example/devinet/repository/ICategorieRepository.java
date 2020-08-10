06297434765e5e7b8fc8f84d06c9c5cbf5892866
sl <s.leyral@gmail.com>
2020-08-10 11:05:24 +0200

    add interface repository

--- app/src/main/java/com/example/devinet/repository/ICategorieRepository.java
+++ app/src/main/java/com/example/devinet/repository/ICategorieRepository.java
@@ -1,4 +1,16 @@
 package com.example.devinet.repository;
 
+import androidx.lifecycle.LiveData;
+
+import com.example.devinet.bo.Categorie;
+
+import java.util.List;
+
 public interface ICategorieRepository {
+    void insert(Categorie item);
+    LiveData<List<Categorie>> get();
+    Categorie get(int id);
+    void update(Categorie item);
+    void delete(Categorie item);
+    void delete();
 }
--- app/src/main/java/com/example/devinet/repository/IMotRepository.java
+++ app/src/main/java/com/example/devinet/repository/IMotRepository.java
@@ -1,4 +1,17 @@
 package com.example.devinet.repository;
 
+import androidx.lifecycle.LiveData;
+
+import com.example.devinet.bo.Mot;
+
+import java.util.List;
+
 public interface IMotRepository {
+
+    void insert(Mot item);
+    LiveData<List<Mot>> get();
+    Mot get(int id);
+    void update(Mot item);
+    void delete(Mot item);
+    void delete();
 }
