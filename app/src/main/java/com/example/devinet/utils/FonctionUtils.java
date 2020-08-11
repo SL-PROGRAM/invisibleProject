fbb2b615e48cdd6550eb98cd95b35254b28e1023
sl <s.leyral@gmail.com>
2020-08-11 16:01:32 +0200

    del thread dans FonctionUtils

--- app/src/main/java/com/example/devinet/utils/FonctionUtils.java
+++ app/src/main/java/com/example/devinet/utils/FonctionUtils.java
@@ -9,9 +9,6 @@ import com.example.devinet.repository.MotBddRepository;
 public class FonctionUtils {
 
     public static final int NB_MOT_PAR_LISTE = 10;
-    private static Context cx = null;
-    private static int niv;
-    private static int numeroListe = 1;
 
 
     public static int longueurMotPourNiveau(String mot){
@@ -19,29 +16,18 @@ public class FonctionUtils {
     }
 
     public static int definirNumeroList(int niveau, Context context){
-        cx = context;
-        niv = niveau;
-
-        Runnable task1 = new Runnable(){
-
-            @Override
-            public void run(){
-                IMotRepository motRepository = new MotBddRepository(cx);
-                Log.i("Test utils", "elemNiv: 0");
-
-                int  nbMotNiveau = motRepository.getMotNiveau(niv).size();
-
-                Log.i("Test utils", "elemNiv: "+ nbMotNiveau);
-                Log.i("Test utils", "list: "+  nbMotNiveau / NB_MOT_PAR_LISTE);
-
-                numeroListe += (nbMotNiveau - 1) / NB_MOT_PAR_LISTE;
-            }
-        };
 
 
-        Thread thread1 = new Thread(task1);
-        thread1.start();
-        return numeroListe;
+        IMotRepository motRepository = new MotBddRepository(context);
+        Log.i("Test utils", "elemNiv: 0");
+
+        int  nbMotNiveau = motRepository.getMotNiveau(niveau).size();
+
+        Log.i("Test utils", "elemNiv: "+ nbMotNiveau);
+        Log.i("Test utils", "list: "+  nbMotNiveau / NB_MOT_PAR_LISTE);
+
+        return  (nbMotNiveau - 1) / NB_MOT_PAR_LISTE + 1;
+
     }
 
 
