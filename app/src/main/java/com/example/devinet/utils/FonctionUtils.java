f615c40ef2383a15d7cbf437c7242dedd00d6f3e
sl <s.leyral@gmail.com>
2020-08-11 12:27:22 +0200

    add fct gestion automatique du numero de la list

--- app/src/main/java/com/example/devinet/utils/FonctionUtils.java
+++ app/src/main/java/com/example/devinet/utils/FonctionUtils.java
@@ -3,30 +3,45 @@ package com.example.devinet.utils;
 import android.content.Context;
 import android.util.Log;
 
-import androidx.lifecycle.LiveData;
-
-import com.example.devinet.bo.Mot;
-import com.example.devinet.repository.CategorieBddRepository;
-import com.example.devinet.repository.ICategorieRepository;
 import com.example.devinet.repository.IMotRepository;
 import com.example.devinet.repository.MotBddRepository;
-
-import java.util.List;
-import java.util.Objects;
 
 public class FonctionUtils {
 
     public static final int NB_MOT_PAR_LISTE = 10;
+    private static Context cx = null;
+    private static int niv;
+    private static int numeroListe = 1;
+
 
     public static int longueurMotPourNiveau(String mot){
         return mot.length();
     }
 
     public static int definirNumeroList(int niveau, Context context){
-        IMotRepository motRepository = new MotBddRepository(context);
-        int nombreDeMotList = Objects.requireNonNull(motRepository.getNiveau(niveau).getValue()).size();
-        Log.i("utils", "definirNumeroList: "+nombreDeMotList);
-        return nombreDeMotList / NB_MOT_PAR_LISTE +1;
+        cx = context;
+        niv = niveau;
+
+        Runnable task1 = new Runnable(){
+
+            @Override
+            public void run(){
+                IMotRepository motRepository = new MotBddRepository(cx);
+                Log.i("Test utils", "elemNiv: 0");
+
+                int  nbMotNiveau = motRepository.getMotNiveau(niv).size();
+
+                Log.i("Test utils", "elemNiv: "+ nbMotNiveau);
+                Log.i("Test utils", "list: "+  nbMotNiveau / NB_MOT_PAR_LISTE);
+
+                numeroListe += (nbMotNiveau - 1) / NB_MOT_PAR_LISTE;
+            }
+        };
+
+
+        Thread thread1 = new Thread(task1);
+        thread1.start();
+        return numeroListe;
     }
 
 
